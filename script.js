512ba2895bab634e145b0884136378b590d750e0
Samjana Lama <samjanalama4262@gmail.com>
2024-12-03 19:34:39 +0545

    Initial commit: added all project files

---
+++ script.js
@@ -0,0 +1,56 @@
+
+document.addEventListener('DOMContentLoaded', function () {
+    const themeToggle = document.getElementById('theme-toggle');
+    const icon = document.getElementById('icon');
+    const body = document.body;
+
+    // Check for saved user preference, if any
+    const currentTheme = localStorage.getItem('theme') ? localStorage.getItem('theme') : 'light';
+    body.classList.add(currentTheme);
+
+    // Set the icon based on saved preference
+    if (currentTheme === 'dark') {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+    }
+
+    // Theme toggle functionality
+    themeToggle.addEventListener('click', function () {
+        body.classList.toggle('dark');
+        body.classList.toggle('light');
+
+        // Change the icon based on the current class
+        if (body.classList.contains('dark')) {
+            icon.classList.remove('fa-sun');
+            icon.classList.add('fa-moon');
+            localStorage.setItem('theme', 'dark');
+        } else {
+            icon.classList.remove('fa-moon');
+            icon.classList.add('fa-sun');
+            localStorage.setItem('theme', 'light');
+        }
+        themeToggle.setAttribute('aria-pressed', body.classList.contains('dark'));
+    });
+
+    // Tab functionality
+    const buttons = document.querySelectorAll('.tab-btn');
+    const contents = document.querySelectorAll('.tab-content');
+
+    // Function to remove 'active' class from all buttons and content
+    function removeActiveClasses() {
+        buttons.forEach(button => button.classList.remove('active'));
+        contents.forEach(content => content.classList.remove('active'));
+    }
+
+    // Add click event listener to each button
+    buttons.forEach(button => {
+        button.addEventListener('click', function () {
+            removeActiveClasses();
+            button.classList.add('active');
+            const tabId = button.getAttribute('data-tab');
+            document.getElementById(tabId).classList.add('active');
+        });
+    });
+});
+
+
